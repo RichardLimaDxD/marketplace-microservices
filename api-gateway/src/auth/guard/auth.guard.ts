5719f3f294c2fcda031a90849fb61a3b9d403468
RichardLimaDxD <richardpires663802@gmail.com>
2026-07-06 14:50:16 -0300

    feat: eslint and session guard

---
+++ api-gateway/src/auth/guard/auth.guard.ts
@@ -0,0 +1,34 @@
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private readonly reflector: Reflector) {
+    super();
+  }
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>('isPublic', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) return true;
+
+    return super.canActivate(context);
+  }
+
+  handleRequest(error, user: any): any {
+    if (error || !user) throw error || new UnauthorizedException();
+
+    return user;
+  }
+}
---
+++ api-gateway/src/auth/guard/session.guard.ts
@@ -0,0 +1,36 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AuthService, UserSession } from '../service/auth.service';
+import { Request } from 'express';
+
+@Injectable()
+export class SessionGuard implements CanActivate {
+  constructor(private readonly authService: AuthService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request: Request = context.switchToHttp().getRequest();
+
+    const sessionToken = request.headers['x-session-token'] as string;
+
+    if (!sessionToken)
+      throw new UnauthorizedException('Session token is required');
+
+    try {
+      const session: UserSession =
+        await this.authService.validateSessionToken(sessionToken);
+
+      if (!session.valid || !session.user)
+        throw new UnauthorizedException('Invalid session');
+
+      (request as unknown as UserSession).user = session.user;
+
+      return true;
+    } catch {
+      throw new UnauthorizedException('Invalid session');
+    }
+  }
+}
