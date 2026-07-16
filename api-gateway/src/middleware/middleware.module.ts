e9335bab52d0a3275abbd9226dab6268fddd515a
RichardLimaDxD <richardpires663802@gmail.com>
2026-07-16 14:34:31 -0300

    chore: middleware module

--- api-gateway/src/middleware/middleware.module.ts
+++ api-gateway/src/middleware/middleware.module.ts
@@ -1,13 +1,15 @@
-import { Module } from "@nestjs/common";
-import { ThrottlerModule } from "@nestjs/throttler";
-import { LoggingMiddleware } from "./logging/logging.middleware";
+import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { LoggingMiddleware } from './logging/logging.middleware';
 
 @Module({
   imports: [
-    ThrottlerModule.forRoot([{
-      ttl: 60,
-      limit: 10,
-    }]),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60,
+        limit: 10,
+      },
+    ]),
   ],
   providers: [LoggingMiddleware],
 })
